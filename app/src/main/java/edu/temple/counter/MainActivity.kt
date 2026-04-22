85f1750b05982a0a8f6f9892b98de40494951d4c
Johnny Tan <tannjohnny03@gmail.com>
2026-04-22 16:33:40 -0400

    Completed assignment

--- app/src/main/java/edu/temple/counter/MainActivity.kt
+++ app/src/main/java/edu/temple/counter/MainActivity.kt
@@ -4,17 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import edu.temple.counter.ui.theme.CounterTheme
 
@@ -36,12 +42,35 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Counter(modifier: Modifier = Modifier) {
-    Column (modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally) {
+    var count by rememberSaveable { mutableStateOf(0) }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        IconButton(
+            onClick = { count++ }
+        ) {
+            Image(
+                painter = painterResource(id = android.R.drawable.arrow_up_float),
+                contentDescription = "Increment counter"
+            )
+        }
+
         Text(
-            text = "0",
+            text = count.toString(),
             fontSize = 192.sp
         )
+
+        IconButton(
+            onClick = { count-- }
+        ) {
+            Image(
+                painter = painterResource(id = android.R.drawable.arrow_down_float),
+                contentDescription = "Decrement counter"
+            )
+        }
     }
 }
 
